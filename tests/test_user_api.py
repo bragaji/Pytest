06f36da5d7c232c4b4bac52d4b81701165d235f8
bragaji <psprasha50@gmail.com>
2025-05-20 03:52:13 -0400

    json print response

--- tests/test_user_api.py
+++ tests/test_user_api.py
@@ -1,3 +1,5 @@
+import json
+
 import requests
 import pytest
 
@@ -7,3 +9,5 @@ def test_get_users():
     response = requests.get(f"{BASE_URL}/users?page=2")
     assert response.status_code == 200
     assert response.json()["page"]==2
+    result = json.dumps(response.json(), indent=4)
+    print(result)
