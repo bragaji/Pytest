bd2f5f8c6c23b078e5d3ec7c86ba344fb032d786
bragaji <psprasha50@gmail.com>
2025-05-21 00:17:13 -0400

    Create/Update requests

--- tests/test_user_api.py
+++ tests/test_user_api.py
@@ -4,10 +4,41 @@ import requests
 import pytest
 
 BASE_URL="https://reqres.in/api"
+HEADERS = {
+    "x-api-key": "reqres-free-v1"
+}
 
 def test_get_users():
-    response = requests.get(f"{BASE_URL}/users?page=2")
+    response = requests.get(f"{BASE_URL}/users?page=2",headers=HEADERS)
     assert response.status_code == 200
     assert response.json()["page"]==2
     result = json.dumps(response.json(), indent=4)
+    print(result)
+
+def test_single_user():
+    response =  requests.get(f"{BASE_URL}/users/2")
+    assert response.status_code ==200
+    assert response.json()["data"]["first_name"]=="Janet"
+    result = json.dumps(response.json(),indent=4)
+    print(result)
+
+def test_create_user():
+    payload ={
+    "name": "morpheus",
+    "job": "leader"
+}
+    response= requests.post(f"{BASE_URL}/users",headers=HEADERS,json=payload)
+    assert response.status_code == 201
+    assert response.json()["name"]=="morpheus"
+    result =json.dumps(response.json(),indent=4)
+    print(result)
+
+def test_update_user():
+    payload ={
+        "name": "Prashant",
+    }
+    response= requests.put(f"{BASE_URL}/users/2",headers=HEADERS,json=payload)
+    assert response.status_code==200
+    assert response.json()["name"]=="Prashant"
+    result = json.dumps(response.json()["name"])
     print(result)
